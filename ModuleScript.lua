21895c32a344b28385f0c52a05f0b62e234184e3
BoringDev <121419504+AProgrammRe@users.noreply.github.com>
2023-05-22 16:10:16 +0700

    Update ModuleScript.lua

--- ModuleScript.lua
+++ ModuleScript.lua
@@ -5,7 +5,7 @@ local viewmodel
 local equipped
 local animations
 local character
-
+local sounds
 
 -- Settings configurations
 function module.SetPlayerCharacter(char)
@@ -19,18 +19,18 @@ end
 function module.setViewmodel(part)
 	viewmodel = part
 end
+function module.SetSoundsFolder(fold)
+	sounds =fold
+end
 
 -- Basic functions
 function module.update()
 	viewmodel.HumanoidRootPart.CFrame = game.Workspace.Camera.CFrame
 end
 
-local equipDebounce
 
 function module.equip(object,debounce)
-	if equipDebounce then
-		return
-	end
+	
 	if equipped then
 		if equipped:FindFirstChild("Animations") then
 			module.SetAnimationsFolder(equipped.Animations)
@@ -39,47 +39,68 @@ function module.equip(object,debounce)
 		local p = controller:LoadAnimation(animations.Change)
 		p.Looped = false
 		p:Play()
-		equipDebounce = true
 		equipped:Destroy()
-		
+		equipped = object:Clone()
+		equipped.Parent = viewmodel
+		module.Weldgun(equipped)
+		module.Weldgun(object,true)
+		equipped.char:Destroy()
+
 		wait(debounce)
-		equipDebounce = false
 		p:Stop()
+	else
+
+		equipped = object:Clone()
+		equipped.Parent = viewmodel
+		module.Weldgun(equipped)
+		module.Weldgun(object,true)
+		equipped.char:Destroy()
 
 	end
-
-	equipped = object:Clone()
-	equipped.Parent = viewmodel
-	module.Weldgun(equipped)
-	module.Weldgun(object,true)
+	
+	module.ClearAnimations()
+	
 	
 	-- run hold animation (viewmodel)
 	local controller = viewmodel.AnimationController
 	local animationLoaded = controller:LoadAnimation(animations.Hold)
 	animationLoaded.Looped = true
 	animationLoaded:Play()
+	animationLoaded.Priority = Enum.AnimationPriority.Core
 	
 	-- run hold animation (server view)
 	local controllerHumanoid = character.Humanoid
 	local animationLoadedForHumanoid = controllerHumanoid:LoadAnimation(animations.HoldServer)
 	animationLoadedForHumanoid.Looped = true
 	animationLoadedForHumanoid:Play()
-	equipped.char:Destroy()
-
+	animationLoadedForHumanoid.Priority = Enum.AnimationPriority.Core
+	
+	
 end
 
-function module.Shoot(target,damage,custom,headshot_damage,torso_damage,arms_damage,legs_damage)
+
+local shootDebounce
+function module.Shoot(target,damage,custom,headshot_damage,torso_damage,arms_damage,legs_damage,debounce)
 	if not equipped then return end
+	if debounce then return end
 	-- run hold animation (viewmodel)
+	character.Humanoid.WalkSpeed = 16
+
 	local controller = viewmodel.AnimationController
 	local animationLoaded = controller:LoadAnimation(animations.Shoot)
+	animationLoaded.Looped = false
 	animationLoaded:Play()
 	-- run for server view
 	local controllerHumanoid = character.Humanoid
 	local animationLoadedForHumanoid = controllerHumanoid:LoadAnimation(animations.ShootServer)
+	animationLoadedForHumanoid.Looped = false
 	animationLoadedForHumanoid:Play()
 	
+	sounds.Shoot:Play()
 	-- Damage
+	if not target then
+		return
+	end
 	if target.Parent == character then
 		return
 	end
@@ -90,11 +111,17 @@ function module.Shoot(target,damage,custom,headshot_damage,torso_damage,arms_dam
 		script.Damage:FireServer(damage,custom,target,headshot_damage,torso_damage,arms_damage,legs_damage)
 	end
 	
+	debounce = true
+	wait(debounce)
+	debounce = false
+	animationLoaded:Stop()
+	animationLoadedForHumanoid:Stop()
 end
 
 function module.FindTargetByRay(distance)
+	if not equipped then return end
 	local ray = Ray.new(equipped.bodyattach.Position,(game.Players:GetPlayerFromCharacter(character):GetMouse().Hit.Position - equipped.bodyattach.Position).Unit*distance)
-	local target,pos = game.Workspace:FindPartOnRay(ray,character,false,true)
+	local target,pos = game.Workspace:FindPartOnRay(ray,viewmodel,false,true)
 	return target
 	
 end
@@ -107,6 +134,62 @@ function module.Weldgun(object, isServer)
 	viewmodel.HumanoidRootPart.Handle.Part1 = object.bodyattach
 end
 
+function module.reset()
+	viewmodel:Destroy()	
+end
+
+function module.viewModelsetup(run,walk,viewModelCustom,runSpeed,keycode)
+	if run then
+		local playing
+		local controller = viewmodel.AnimationController
+		local runViewmodel = controller:LoadAnimation(animations.Run)
+		runViewmodel.Looped = true
+		game:GetService("RunService").RenderStepped:Connect(function()
+			
+			if character then
+				if character.Humanoid.MoveDirection.Magnitude > 0 and character.Humanoid.WalkSpeed >= runSpeed then
+					if playing then return end
+					playing = true
+					
+					runViewmodel:Play()
+
+				else
+					playing = false
+					runViewmodel:Stop()
+					
+				end
+			end
+		end)
+		game:GetService("UserInputService").InputBegan:Connect(function(input)
+			if input.KeyCode == keycode then
+				character.Humanoid.WalkSpeed = runSpeed
+			end
+		end)
+
+		game:GetService("UserInputService").InputEnded:Connect(function(input)
+			if input.KeyCode == keycode then
+				character.Humanoid.WalkSpeed = 16
+			end
+		end)
+	end
+	if walk then
+		warn("This is under development!")
+	end
+	
+end
+
+
+function module.ClearAnimations(humanoid)
+	if humanoid then
+		for _, animation in ipairs(viewmodel.AnimationController:GetPlayingAnimationTracks()) do
+			if animation.Priority == Enum.AnimationPriority.Core then --Only target action priority animations.
+			else
+				animation:Stop()
+
+			end
+		end
+	end
+end
 
 -- Init server for module to access module sending client info to server. Informing the server events
 function module.InitServer()
@@ -154,3 +237,4 @@ end
 
 -- return module
 return module
+
